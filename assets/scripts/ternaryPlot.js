5f068fb07e81a8a88db227d9b0e3d71ef16c72a8
Bey Hao Yun <beyhy94@gmail.com>
2024-04-09 01:03:54 +0800

    :hammer: Added floating animation. Still need to include interactability feature.
    Signed-off-by: Bey Hao Yun <beyhy94@gmail.com>

--- assets/scripts/ternaryPlot.js
+++ assets/scripts/ternaryPlot.js
@@ -12,206 +12,163 @@ rogue_color = 'blue';
 
 const border_padding = 30
 
-// Define dictionary of character classes
-const textArray = [
-  ["Thief", "Cat Burglar", "Stalker", "Trickster", "Shadowmancer", "Binder", "Illusionist", "Stuv", "Witch", "Wizard"], // Bottommost row with text strings
-  ["This", "is", "row", "2"], // Second row with text strings
-  // Add more rows as needed
-];
-
 // Draw the labelled node
 function drawLabelledNode(x, y, radius, color, labelText) {
   ctx.beginPath();
   ctx.arc(x, y, radius, 0, Math.PI * 2, false); // Full circle
   ctx.fillStyle = color;
   ctx.fill();
+
   labelFontSize = 10;
   labelYOffset = radius + labelFontSize + 5;
   ctx.font = `${labelFontSize}px Arial`;
   ctx.fillStyle = 'white';
   ctx.textAlign = 'center';
   ctx.fillText(labelText, x, y + labelYOffset);
+}
 
-  // ctx.addEventListener('mousemove', (event) => {
-  //   labelFontSize = 8;
-  //   labelYOffset = radius + labelFontSize + 5;
-  //   ctx.font = `${labelFontSize}px Arial`;
-  //   ctx.fillStyle = 'white';
-  //   ctx.textAlign = 'center';
-  //   ctx.fillText(labelText, x, y + labelYOffset);
-  // });
+// Function to animate a floating node
+function animateFloatingNode() {
+  let yOffset = 0;
+
+  function drawFrame() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    // Add a slight wave-like motion
+    yOffset = (Math.sin(Date.now() / 500)) + 10;
+
+    // Draw pillar nodes
+    drawLabelledNode(30, 730 + yOffset, rogue_radius, rogue_color, 'ROGUE');
+    drawLabelledNode(730, 730 + yOffset, mage_radius, mage_color, 'MAGE');
+    drawLabelledNode(380, 30 + yOffset, fighter_radius, fighter_color, 'FIGHTER');
+
+    y_step = 35
+
+    middle_pos_x = 380
+    x_buffer = 36
+    start_pos_y = 700
+    drawLabelledNode((middle_pos_x - (x_buffer * 9)), start_pos_y + yOffset, 5, 'grey', "Thief")
+    drawLabelledNode((middle_pos_x - x_buffer * 7), start_pos_y + yOffset, 5, 'grey', "Cat Burglar")
+    drawLabelledNode((middle_pos_x - x_buffer * 5), start_pos_y + yOffset, 5, 'grey', "Stalker")
+    drawLabelledNode((middle_pos_x - x_buffer * 3), start_pos_y + yOffset, 5, 'grey', "Trickster")
+    drawLabelledNode((middle_pos_x - x_buffer * 1), start_pos_y + yOffset, 5, 'grey', "Shadowmancer")
+    drawLabelledNode((middle_pos_x + x_buffer * 1), start_pos_y + yOffset, 5, 'grey', "Binder")
+    drawLabelledNode((middle_pos_x + x_buffer * 3), start_pos_y + yOffset, 5, 'grey', "Illusionist")
+    drawLabelledNode((middle_pos_x + x_buffer * 5), start_pos_y + yOffset, 5, 'grey', "Stuv")
+    drawLabelledNode((middle_pos_x + x_buffer * 7), start_pos_y + yOffset, 5, 'grey', "Witch")
+    drawLabelledNode((middle_pos_x + (x_buffer * 9)), start_pos_y + yOffset, 5, 'grey', "Wizard")
+    start_pos_y = 700 - y_step
+    drawLabelledNode((middle_pos_x - (x_buffer * 8)), start_pos_y + yOffset, 5, 'grey', "Infiltrator")
+    drawLabelledNode((middle_pos_x - x_buffer * 6), start_pos_y + yOffset, 5, 'grey', "Philanderer")
+    drawLabelledNode((middle_pos_x - x_buffer * 4), start_pos_y + yOffset, 5, 'grey', "Chemiston")
+    drawLabelledNode((middle_pos_x - x_buffer * 2), start_pos_y + yOffset, 5, 'grey', "Magician")
+    drawLabelledNode((middle_pos_x), start_pos_y + yOffset, 5, 'grey', "Alchemist")
+    drawLabelledNode((middle_pos_x + x_buffer * 2), start_pos_y + yOffset, 5, 'grey', "Conjurer")
+    drawLabelledNode((middle_pos_x + x_buffer * 4), start_pos_y + yOffset, 5, 'grey', "Shaman")
+    drawLabelledNode((middle_pos_x + x_buffer * 6), start_pos_y + yOffset, 5, 'grey', "Arsonist")
+    drawLabelledNode((middle_pos_x + (x_buffer * 8)), start_pos_y + yOffset, 5, 'grey', "Sorcerer")
+    start_pos_y = 700 - (y_step * 2)
+    drawLabelledNode((middle_pos_x - (x_buffer * 8)), start_pos_y + yOffset, 5, 'grey', "Highway Man")
+    drawLabelledNode((middle_pos_x - x_buffer * 6), start_pos_y + yOffset, 5, 'grey', "Spy")
+    drawLabelledNode((middle_pos_x - x_buffer * 4), start_pos_y + yOffset, 5, 'grey', "Trap Master")
+    drawLabelledNode((middle_pos_x - x_buffer * 2), start_pos_y + yOffset, 5, 'grey', "Gambler")
+    drawLabelledNode((middle_pos_x), start_pos_y + yOffset, 5, 'grey', "Tinker")
+    drawLabelledNode((middle_pos_x + x_buffer * 2), start_pos_y + yOffset, 5, 'grey', "Animist")
+    drawLabelledNode((middle_pos_x + x_buffer * 4), start_pos_y + yOffset, 5, 'grey', "Dreamwalker")
+    drawLabelledNode((middle_pos_x + x_buffer * 6), start_pos_y + yOffset, 5, 'grey', "Sage")
+    drawLabelledNode((middle_pos_x + (x_buffer * 8)), start_pos_y + yOffset, 5, 'grey', "Warlock")
+    start_pos_y = 700 - (y_step * 3)
+    drawLabelledNode((middle_pos_x - (x_buffer * 7)), start_pos_y + yOffset, 5, 'grey', "Ballistician")
+    drawLabelledNode((middle_pos_x - x_buffer * 5), start_pos_y + yOffset, 5, 'grey', "Poisoner")
+    drawLabelledNode((middle_pos_x - x_buffer * 3), start_pos_y + yOffset, 5, 'grey', "Dark Delver")
+    drawLabelledNode((middle_pos_x - x_buffer * 1), start_pos_y + yOffset, 5, 'grey', "Mentalist")
+    drawLabelledNode((middle_pos_x + x_buffer * 1), start_pos_y + yOffset, 5, 'grey', "Technomancer")
+    drawLabelledNode((middle_pos_x + x_buffer * 3), start_pos_y + yOffset, 5, 'grey', "Geomancer")
+    drawLabelledNode((middle_pos_x + x_buffer * 5), start_pos_y + yOffset, 5, 'grey', "Channeler")
+    drawLabelledNode((middle_pos_x + (x_buffer * 7)), start_pos_y + yOffset, 5, 'grey', "Theurge")
+    start_pos_y = 700 - (y_step * 4)
+    drawLabelledNode((middle_pos_x - (x_buffer * 7)), start_pos_y + yOffset, 5, 'grey', "Cat Purse")
+    drawLabelledNode((middle_pos_x - x_buffer * 5), start_pos_y + yOffset, 5, 'grey', "Hunter")
+    drawLabelledNode((middle_pos_x - x_buffer * 3), start_pos_y + yOffset, 5, 'grey', "Beastmaster")
+    drawLabelledNode((middle_pos_x - x_buffer * 1), start_pos_y + yOffset, 5, 'grey', "Serider")
+    drawLabelledNode((middle_pos_x + x_buffer * 1), start_pos_y + yOffset, 5, 'grey', "Enchanter")
+    drawLabelledNode((middle_pos_x + x_buffer * 3), start_pos_y + yOffset, 5, 'grey', "Psion")
+    drawLabelledNode((middle_pos_x + x_buffer * 5), start_pos_y + yOffset, 5, 'grey', "Necromancer")
+    drawLabelledNode((middle_pos_x + (x_buffer * 7)), start_pos_y + yOffset, 5, 'grey', "Acolyte")
+    start_pos_y = 700 - (y_step * 5)
+    drawLabelledNode((middle_pos_x - (x_buffer * 6)), start_pos_y + yOffset, 5, 'grey', "Scout")
+    drawLabelledNode((middle_pos_x - x_buffer * 4), start_pos_y + yOffset, 5, 'grey', "Explorer")
+    drawLabelledNode((middle_pos_x - x_buffer * 2), start_pos_y + yOffset, 5, 'grey', "Horizon Walker")
+    drawLabelledNode((middle_pos_x), start_pos_y + yOffset, 5, 'grey', "Lore Master")
+    drawLabelledNode((middle_pos_x + x_buffer * 2), start_pos_y + yOffset, 5, 'grey', "Summoner")
+    drawLabelledNode((middle_pos_x + x_buffer * 4), start_pos_y + yOffset, 5, 'grey', "Elementalist")
+    drawLabelledNode((middle_pos_x + (x_buffer * 6)), start_pos_y + yOffset, 5, 'grey', "Druid")
+    start_pos_y = 700 - (y_step * 6)
+    drawLabelledNode((middle_pos_x - (x_buffer * 6)), start_pos_y + yOffset, 5, 'grey', "Bandit")
+    drawLabelledNode((middle_pos_x - x_buffer * 4), start_pos_y + yOffset, 5, 'grey', "Assassin")
+    drawLabelledNode((middle_pos_x - x_buffer * 2), start_pos_y + yOffset, 5, 'grey', "Ninja")
+    drawLabelledNode((middle_pos_x), start_pos_y + yOffset, 5, 'grey', "Bard")
+    drawLabelledNode((middle_pos_x + x_buffer * 2), start_pos_y + yOffset, 5, 'grey', "Arcane Archer")
+    drawLabelledNode((middle_pos_x + x_buffer * 4), start_pos_y + yOffset, 5, 'grey', "Blue Mage")
+    drawLabelledNode((middle_pos_x + (x_buffer * 6)), start_pos_y + yOffset, 5, 'grey', "Battle Priest")
+    start_pos_y = 700 - (y_step * 7)
+    drawLabelledNode((middle_pos_x - (x_buffer * 5)), start_pos_y + yOffset, 5, 'grey', "Gunslinger")
+    drawLabelledNode((middle_pos_x - x_buffer * 3), start_pos_y + yOffset, 5, 'grey', "Ranger")
+    drawLabelledNode((middle_pos_x - x_buffer * 1), start_pos_y + yOffset, 5, 'grey', "Merchant")
+    drawLabelledNode((middle_pos_x + x_buffer * 1), start_pos_y + yOffset, 5, 'grey', "Dancer")
+    drawLabelledNode((middle_pos_x + x_buffer * 3), start_pos_y + yOffset, 5, 'grey', "Red Mage")
+    drawLabelledNode((middle_pos_x + (x_buffer * 5)), start_pos_y + yOffset, 5, 'grey', "Clerk")
+    start_pos_y = 700 - (y_step * 8)
+    drawLabelledNode((middle_pos_x - (x_buffer * 5)), start_pos_y + yOffset, 5, 'grey', "Marksman")
+    drawLabelledNode((middle_pos_x - x_buffer * 3), start_pos_y + yOffset, 5, 'grey', "Marauder")
+    drawLabelledNode((middle_pos_x - x_buffer * 1), start_pos_y + yOffset, 5, 'grey', "Adventurer")
+    drawLabelledNode((middle_pos_x + x_buffer * 1), start_pos_y + yOffset, 5, 'grey', "Diplomat")
+    drawLabelledNode((middle_pos_x + x_buffer * 3), start_pos_y + yOffset, 5, 'grey', "Spellsword")
+    drawLabelledNode((middle_pos_x + (x_buffer * 5)), start_pos_y + yOffset, 5, 'grey', "Battle Mage")
+    start_pos_y = 700 - (y_step * 9)
+    drawLabelledNode((middle_pos_x - (x_buffer * 4)), start_pos_y + yOffset, 5, 'grey', "Duelist")
+    drawLabelledNode((middle_pos_x - x_buffer * 2), start_pos_y + yOffset, 5, 'grey', "Raider")
+    drawLabelledNode((middle_pos_x), start_pos_y + yOffset, 5, 'grey', "Strategist")
+    drawLabelledNode((middle_pos_x + x_buffer * 2), start_pos_y + yOffset, 5, 'grey', "Templar")
+    drawLabelledNode((middle_pos_x + (x_buffer * 4)), start_pos_y + yOffset, 5, 'grey', "Death Knight")
+    start_pos_y = 700 - (y_step * 10)
+    drawLabelledNode((middle_pos_x - (x_buffer * 4)), start_pos_y + yOffset, 5, 'grey', "Archer")
+    drawLabelledNode((middle_pos_x - x_buffer * 2), start_pos_y + yOffset, 5, 'grey', "Beast Rider")
+    drawLabelledNode((middle_pos_x), start_pos_y + yOffset, 5, 'grey', "Warlord")
+    drawLabelledNode((middle_pos_x + x_buffer * 2), start_pos_y + yOffset, 5, 'grey', "Inquisitor")
+    drawLabelledNode((middle_pos_x + (x_buffer * 4)), start_pos_y + yOffset, 5, 'grey', "Paladin")
+    start_pos_y = 700 - (y_step * 11)
+    drawLabelledNode((middle_pos_x - (x_buffer * 3)), start_pos_y + yOffset, 5, 'grey', "Corsair")
+    drawLabelledNode((middle_pos_x - x_buffer * 1), start_pos_y + yOffset, 5, 'grey', "Ronin")
+    drawLabelledNode((middle_pos_x + x_buffer * 1), start_pos_y + yOffset, 5, 'grey', "Herald")
+    drawLabelledNode((middle_pos_x + (x_buffer * 3)), start_pos_y + yOffset, 5, 'grey', "Blade Singer")
+    start_pos_y = 700 - (y_step * 12)
+    drawLabelledNode((middle_pos_x - (x_buffer * 3)), start_pos_y + yOffset, 5, 'grey', "Fencer")
+    drawLabelledNode((middle_pos_x - x_buffer * 1), start_pos_y + yOffset, 5, 'grey', "Samurai")
+    drawLabelledNode((middle_pos_x + x_buffer * 1), start_pos_y + yOffset, 5, 'grey', "Monk")
+    drawLabelledNode((middle_pos_x + (x_buffer * 3)), start_pos_y + yOffset, 5, 'grey', "Dark Knight")
+    start_pos_y = 700 - (y_step * 13)
+    drawLabelledNode((middle_pos_x - x_buffer * 2), start_pos_y + yOffset, 5, 'grey', "Soldier")
+    drawLabelledNode((middle_pos_x), start_pos_y + yOffset, 5, 'grey', "Martial Artist")
+    drawLabelledNode((middle_pos_x + x_buffer * 2), start_pos_y + yOffset, 5, 'grey', "Dragon Knight")
+    start_pos_y = 700 - (y_step * 14)
+    drawLabelledNode((middle_pos_x - x_buffer * 2), start_pos_y + yOffset, 5, 'grey', "Pugilist")
+    drawLabelledNode((middle_pos_x), start_pos_y + yOffset, 5, 'grey', "Weapon Master")
+    drawLabelledNode((middle_pos_x + x_buffer * 2), start_pos_y + yOffset, 5, 'grey', "Sentinel")
+    start_pos_y = 700 - (y_step * 15)
+    drawLabelledNode((middle_pos_x - x_buffer), start_pos_y + yOffset, 5, 'grey', "Shield Bearer")
+    drawLabelledNode((middle_pos_x + x_buffer), start_pos_y + yOffset, 5, 'grey', "Dragoon")
+    start_pos_y = 700 - (y_step * 16)
+    drawLabelledNode((middle_pos_x - x_buffer), start_pos_y + yOffset, 5, 'grey', "Cavalier")
+    drawLabelledNode((middle_pos_x + x_buffer), start_pos_y + yOffset, 5, 'grey', "Berserker")
+    drawLabelledNode(middle_pos_x, 700 - (y_step * 17) + yOffset, 5, 'grey', "Knight")
+    drawLabelledNode(middle_pos_x, 700 - (y_step * 18) + yOffset, 5, 'grey', "Warrior")
+
+    requestAnimationFrame(drawFrame);
+  }
+
+  drawFrame();
 }
 
 // Main nodes
-drawLabelledNode(30, 730, mage_radius, mage_color, 'MAGE');
-drawLabelledNode(740, 730, rogue_radius, rogue_color, 'ROGUE');
-
-// limit = 10
-// total_plot_height = 700
-// start_pos_x = 30
-// start_pos_y = 730 - total_plot_height/limit
-// x_offset = total_plot_height/limit/2
-// for (let j = 0; j <= limit; j++) {
-//   for (let i = 1; i <= limit - j; i++) {
-//     drawLabelledNode(start_pos_x + i * 710/(limit + 1) + j * x_offset, start_pos_y - j *(total_plot_height/limit - 2), 5, 'grey', '-');
-//   }
-// }
-
-y_step = 35
-x_step = 32
-x_sub_step = 64
-
-array1 = ["Thief", "Cat Burglar", "Stalker", "Trickster", "Shadowmancer", "Binder", "Illusionist", "Stuv", "Witch", "Wizard"];
-start_pos_x = x_step
-start_pos_y = 700 - (y_step * 0)
-limit = 10
-for (let i = 1; i <= limit; i++) {
-  drawLabelledNode(start_pos_x + i * x_sub_step, start_pos_y, 5, 'grey', array1[i-1])
-}
-
-array2 = ["Infiltrator", "Philanderer", "Chemiston", "Magician", "Alchemist", "Conjurer", "Shaman", "Arsonist", "Sorcerer"];
-start_pos_x = x_step * 2
-start_pos_y = 700 - (y_step * 1)
-limit = 9
-x_offset = 5
-for (let i = 1; i <= limit; i++) {
-  drawLabelledNode((start_pos_x + i * x_sub_step), start_pos_y, 5, 'grey', array2[i-1])
-}
-
-array3 = ["Highway Man", "Spy", "Trap Master", "Gambler", "Tinker", "Animist", "Dreamwalker", "Sage", "Warlock"];
-start_pos_x = x_step * 2
-start_pos_y = 700 - (y_step * 2)
-limit = 9
-for (let i = 1; i <= limit; i++) {
-  drawLabelledNode((start_pos_x + i * x_sub_step), start_pos_y, 5, 'grey', array3[i-1])
-}
-
-array = ["Ballistician", "Poisoner", "Dark Delver", "Mentalist", "Technomancer", "Geomancer", "Channeler", "Theurge"];
-start_pos_x = x_step * 3
-start_pos_y = 700 - (y_step * 3)
-limit = 8
-for (let i = 1; i <= limit; i++) {
-  drawLabelledNode((start_pos_x + i * x_sub_step), start_pos_y, 5, 'grey', array[i-1])
-}
-
-array = ["Cat Purse", "Hunter", "Beastmaster", "Serider", "Enchanter", "Psion", "Necromancer", "Acolyte"];
-start_pos_x = x_step * 3
-start_pos_y = 700 - (y_step * 4)
-limit = 8
-for (let i = 1; i <= limit; i++) {
-  drawLabelledNode((start_pos_x + i * x_sub_step), start_pos_y, 5, 'grey', array[i-1])
-}
-
-array = ["Scout", "Explorer", "Horizon Walker", "Lore Master", "Summoner", "Elementalist", "Druid"];
-start_pos_x = x_step * 4
-start_pos_y = 700 - (y_step * 5)
-limit = 7
-for (let i = 1; i <= limit; i++) {
-  drawLabelledNode((start_pos_x + i * x_sub_step), start_pos_y, 5, 'grey', array[i-1])
-}
-
-array = ["Bandit", "Assassin", "Ninja", "Bard", "Arcane Archer", "Blue Mage", "Battle Priest"];
-start_pos_x = x_step * 4
-start_pos_y = 700 - (y_step * 6)
-limit = 7
-for (let i = 1; i <= limit; i++) {
-  drawLabelledNode((start_pos_x + i * x_sub_step), start_pos_y, 5, 'grey', array[i-1])
-}
-
-array = ["Gunslinger", "Ranger", "Merchant", "Dancer", "Red Mage", "Clerk"];
-start_pos_x = x_step * 5
-start_pos_y = 700 - (y_step * 7)
-limit = 6
-for (let i = 1; i <= limit; i++) {
-  drawLabelledNode((start_pos_x + i * x_sub_step), start_pos_y, 5, 'grey', array[i-1])
-}
-
-array = ["Marksman", "Marauder", "Adventurer", "Diplomat", "Spellsword", "Battle Mage"];
-start_pos_x = x_step * 5
-start_pos_y = 700 - (y_step * 8)
-limit = 6
-for (let i = 1; i <= limit; i++) {
-  drawLabelledNode((start_pos_x + i * x_sub_step), start_pos_y, 5, 'grey', array[i-1])
-}
-
-array = ["Duelist", "Raider", "Strategist", "Templar", "Death Knight"];
-start_pos_x = x_step * 6
-start_pos_y = 700 - (y_step * 9)
-limit = 5
-for (let i = 1; i <= limit; i++) {
-  drawLabelledNode((start_pos_x + i * x_sub_step), start_pos_y, 5, 'grey', array[i-1])
-}
-
-array = ["Archer", "Beast Rider", "Warlord", "Inquisitor", "Paladin"];
-start_pos_x = x_step * 6
-start_pos_y = 700 - (y_step * 10)
-limit = 5
-for (let i = 1; i <= limit; i++) {
-  drawLabelledNode((start_pos_x + i * x_sub_step), start_pos_y, 5, 'grey', array[i-1])
-}
-
-array = ["Corsair", "Ronin", "Herald", "Blade Singer"];
-start_pos_x = x_step * 7
-start_pos_y = 700 - (y_step * 11)
-limit = 4
-for (let i = 1; i <= limit; i++) {
-  drawLabelledNode((start_pos_x + i * x_sub_step), start_pos_y, 5, 'grey', array[i-1])
-}
-
-array = ["Fencer", "Samurai", "Monk", "Dark Knight"];
-start_pos_x = x_step * 7
-start_pos_y = 700 - (y_step * 12)
-limit = 4
-for (let i = 1; i <= limit; i++) {
-  drawLabelledNode((start_pos_x + i * x_sub_step), start_pos_y, 5, 'grey', array[i-1])
-}
-
-array = ["Soldier", "Martial Artist", "Dragon Knight"];
-start_pos_x = x_step * 8
-start_pos_y = 700 - (y_step * 13)
-limit = 3
-for (let i = 1; i <= limit; i++) {
-  drawLabelledNode((start_pos_x + i * x_sub_step), start_pos_y, 5, 'grey', array[i-1])
-}
-
-array = ["Pugilist", "Weapon Master", "Sentinel"];
-start_pos_x = x_step * 8
-start_pos_y = 700 - (y_step * 14)
-limit = 3
-for (let i = 1; i <= limit; i++) {
-  drawLabelledNode((start_pos_x + i * x_sub_step), start_pos_y, 5, 'grey', array[i-1])
-}
-
-array = ["Shield Bearer", "Dragoon"];
-start_pos_x = x_step * 9
-start_pos_y = 700 - (y_step * 15)
-limit = 2
-for (let i = 1; i <= limit; i++) {
-  drawLabelledNode((start_pos_x + i * x_sub_step), start_pos_y, 5, 'grey', array[i-1])
-}
-
-array = ["Cavalier", "Berserker"];
-start_pos_x = x_step * 9
-start_pos_y = 700 - (y_step * 16)
-limit = 2
-for (let i = 1; i <= limit; i++) {
-  drawLabelledNode((start_pos_x + i * x_sub_step), start_pos_y, 5, 'grey', array[i-1])
-}
-
-array = ["Knight"];
-start_pos_x = x_step * 10
-start_pos_y = 700 - (y_step * 17)
-limit = 1
-for (let i = 1; i <= limit; i++) {
-  drawLabelledNode((start_pos_x + i * x_sub_step), start_pos_y, 5, 'grey', array[i-1])
-}
-
-array = ["Warrior"];
-start_pos_x = x_step * 10
-start_pos_y = 700 - (y_step * 18)
-limit = 1
-for (let i = 1; i <= limit; i++) {
-  drawLabelledNode((start_pos_x + i * x_sub_step), start_pos_y, 5, 'grey', array[i-1])
-}
-
-drawLabelledNode(x_step * 10 + x_sub_step, 700 - (y_step * 19), fighter_radius, fighter_color, 'FIGHTER');
+animateFloatingNode();
